036beafe6bb3bc45ebc97196c5294766d539c2b0
Aman Gaur <amangaur6515@gmail.com>
2024-04-25 23:48:34 +0530

    bottom tab navigation added

--- App.js
+++ App.js
@@ -7,10 +7,17 @@ import AddContacts from './screens/AddContacts';
 import { useEffect } from 'react';
 import db,{initDatabase} from './db/db';
 import { useState } from 'react';
-import FavouriteIcon from './components/FavouriteIcon';
+import FavouriteIcon from './components/FavouriteIcon'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import StackNavigator from './components/StackNavigator';
+import Favourites from './screens/Favourites';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
-
+const Tab = createMaterialBottomTabNavigator();
 const Stack=createNativeStackNavigator();
+
+
 
 export default function App() {
   const [contacts, setContacts] = useState([]);
@@ -29,47 +36,76 @@ export default function App() {
   }, []);
   return ( 
     <>
-    <StatusBar backgroundColor="#1a75ff" />
-   <NavigationContainer> 
-      <Stack.Navigator 
-        screenOptions={
-          {
-            headerTintColor:"#1a75ff",
-            headerStyle:{
-              backgroundColor:"white"
+      <StatusBar backgroundColor="#1a75ff" />
+      <NavigationContainer> 
+          {/* <Stack.Navigator 
+            screenOptions={
+              {
+                headerTintColor:"#1a75ff",
+                headerStyle:{
+                  backgroundColor:"white"
+                }
+              } 
             }
-          } 
-        }
-        
-        
-      >
-        <Stack.Screen name='Contacts List' component={ContactsList} 
-          options={{
-            headerTitle: () => (
-              <SearchBar/>
-            ),
-          }}
-        />
+            
+            
+          >
+            <Stack.Screen name='Contacts List' component={ContactsList} 
+              options={{
+                headerTitle: () => (
+                  <SearchBar/>
+                ),
+              }}
+            />
 
-        <Stack.Screen name='Add Contacts' component={AddContacts} 
-          options={
-            {
-              headerRight:()=>{
-                return(
-                  <FavouriteIcon/>
-                );
+            <Stack.Screen name='Add Contacts' component={AddContacts} 
+              options={
+                {
+                  headerRight:()=>{
+                    return(
+                      <FavouriteIcon/>
+                    );
+                  }
+                }
               }
-            }
-          }
-        />
-        
-      </Stack.Navigator>
-   </NavigationContainer>
+            />
+            
+          </Stack.Navigator> */}
+
+          <Tab.Navigator
+            barStyle={{ backgroundColor: "white", activeBackgroundColor: "white" }} // Set activeBackgroundColor to transparent
+            activeColor="#ffffff"
+            shifting={true}
+            
+          >
+            <Tab.Screen name='Contacts' component={StackNavigator}
+            options={{
+              tabBarLabel:"",
+              tabBarIcon: ({ color }) => (
+                <MaterialIcons name="contacts" size={30} color="blue" style={styles.tabBarIcon} />
+              )
+            }}
+             />
+            <Tab.Screen name='Favourites' component={Favourites} 
+              options={{
+                tabBarLabel:"",
+                tabBarIcon: ({ color }) => (
+                  <MaterialIcons name="favorite" size={30} color="blue" style={styles.tabBarIcon}/>
+                )
+              }}
+            />
+          </Tab.Navigator>
+      </NavigationContainer>
    </>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  tabBarIcon: {
+    width: 100, // Set the width to increase icon size
+    height: 100, // Set the height to increase icon size
+    textAlign: 'center', // Center the icon horizontally
+    lineHeight: 50, // Center the icon vertically
+  },
   
 });
